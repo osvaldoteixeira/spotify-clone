d9584f4c371dc77673ba560a3708a291eccb8af6
Osvaldo Bastros Teixeira Junior <105314486+osvaldoteixeira@users.noreply.github.com>
2025-04-23 13:29:51 -0300

    edita o layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
-import Sidebar from "../../../components/Sidebar";
+import Sidebar from "../../components/Sidebar";
 
 const font = Figtree({
   subsets: ["latin"],
@@ -20,9 +20,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={font.className}>
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <Sidebar>{children}</Sidebar>
       </body>
     </html>
   );
